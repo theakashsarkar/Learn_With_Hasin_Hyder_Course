448afd4ef77ba2d7face0072144bd76c461fbf29
theakashsarkar <theakashsarkar7@gmail.com>
2019-07-29 05:25:09 -0700

    source code

--- constructor/index.js
+++ constructor/index.js
@@ -8,3 +8,17 @@ function CreateHuman(name,age,gender){
 var a = CreateHuman("akash","398","M");
 var b =  CreateHuman("sujon","98","m");
 console.log(a,b);
+/********************************************* */
+//consotre function
+
+function Human(name,age,gender){
+        this. name =name;
+        this.age   = age;
+        this.gender= gender;
+
+    
+}
+var a = new Human("akash",20,"M");
+var b = new Human('sujon',89,'M');
+console.log(a);
+console.log(b);
