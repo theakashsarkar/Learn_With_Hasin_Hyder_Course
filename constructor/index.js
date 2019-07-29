f97c39c4b356a79c4c07732d7710840a31104ca9
theakashsarkar <theakashsarkar7@gmail.com>
2019-07-29 00:11:20 -0700

    source code

---
+++ constructor/index.js
@@ -0,0 +1,10 @@
+function CreateHuman(name,age,gender){
+    return{
+        name:name,
+        age:age,
+        gender:gender
+    }
+}
+var a = CreateHuman("akash","398","M");
+var b =  CreateHuman("sujon","98","m");
+console.log(a,b);
